acca91d99c51ad47c185b80c77b31b98181b3048
Daniel Tan <dtch009@gmail.com>
2021-10-17 01:42:04 +0800

    Add basic test for FractalFFT

---
+++ tests/test_fractal_fft.py
@@ -0,0 +1,13 @@
+"""Test suite for FractalFFT."""
+import numpy as np
+
+from fractal_fft.fractal_fft import FractalFFT
+
+
+def test_fractal_fft():
+    """Check that FractalFFT runs with no errors on a toy example."""
+    A = 2 * np.identity(2, dtype=np.int32)
+    B = np.identity(2, dtype=np.int32)
+    C = np.identity(2, dtype=np.int32)
+    fractal_fft = FractalFFT(A, B, C)
+    _ = fractal_fft.M(2)
