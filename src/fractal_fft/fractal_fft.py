a46d2018ae67c9ff27b01e55c94066424c09a49a
Daniel Tan <dtch009@gmail.com>
2021-10-17 00:50:47 +0800

    Ensure linters pass

--- src/fractal_fft/fractal_fft.py
+++ src/fractal_fft/fractal_fft.py
@@ -1,19 +1,24 @@
+"""Code for FractalFFT implementation."""
 import numpy as np
 
 TWO_PI = 2 * np.pi
 
-class FractalFFT:
-    """ 
-    Implementation of Fractal FFT method described in
-    'A Fast Fourier Transform for Fractal Approximations'
-    Reference: https://arxiv.org/pdf/1607.03690.pdf
 
-    Args:
-        :param A: d x d matrix
-        :param B: K x d matrix, each column is a vector
-        :param C: K x d matrix, each column is a vector
+class FractalFFT:
+    """Implementation of Fractal FFT algorithm.
+
+    Described in 'A Fast Fourier Transform for Fractal Approximations'
+    Reference: https://arxiv.org/pdf/1607.03690.pdf
     """
+
     def __init__(self, A: np.ndarray, B: np.ndarray, C: np.ndarray):
+        """Initializes FractalFFT.
+
+        Args:
+            A: d x d matrix
+            B: K x d matrix, each column is a vector
+            C: K x d matrix, each column is a vector
+        """
         # TODO: error handling
         # TODO: check Hadamard, invertible
         self.A = A
@@ -23,20 +28,22 @@ class FractalFFT:
         self.K = B.shape[0]
 
     def _normalize(self):
-        """ Normalize to ensure b_0, c_0 are 0 """
-        self.B = self.B - self.B[:,0]
-        self.C = self.C - self.C[:,0]
+        """Normalize to ensure b_0, c_0 are 0."""
+        self.B = self.B - self.B[:, 0]
+        self.C = self.C - self.C[:, 0]
 
     def E(self, N, m):
+        """Calculate matrix E."""
         # TODO: implement caching to reduce computation
         return self._E(N, m)
 
     def _E(self, N, m):
-        e = self.C.T @ (self.A ** N) @ self.B[:,m]
+        e = self.C.T @ (self.A ** N) @ self.B[:, m]
         e = np.stack([e] * self.K, axis=1)
         return np.exp(-TWO_PI * 1j * e)
 
     def D(self, N, m):
+        """Calculate matrix D."""
         # TODO: implement caching to reduce computation
         return self._D(N, m)
 
@@ -47,9 +54,10 @@ class FractalFFT:
         elif N == 1:
             return 1
         else:
-            return np.kron(self.D(N-1, m), self.E(N, m))
+            return np.kron(self.D(N - 1, m), self.E(N, m))
 
     def M(self, N):
+        """Calculate matrix M."""
         # TODO: implement caching to reduce computation
         return self._M(N)
 
@@ -59,25 +67,30 @@ class FractalFFT:
             return np.exp(TWO_PI * 1j * m)
         else:
             M_1 = self.M(1)
-            M_prev = self.M(N-1)
+            M_prev = self.M(N - 1)
             dim_prev = M_prev.shape[0]
-            M_N = np.zeros(shape = (dim_prev * self.K, dim_prev * self.K), dtype = np.csingle)
+            M_N = np.zeros(
+                shape=(dim_prev * self.K, dim_prev * self.K), dtype=np.csingle
+            )
             # Calculate M_N blockwise
             for col in range(self.K):
-                col_start_idx = col * dim_prev 
+                col_start_idx = col * dim_prev
                 col_end_idx = (col + 1) * dim_prev
                 D_N_col_M_prev = self.D(N, col) @ M_prev
                 for row in range(self.K):
-                    row_start_idx = row * dim_prev 
+                    row_start_idx = row * dim_prev
                     row_end_idx = (row + 1) * dim_prev
-                    M_N[row_start_idx:row_end_idx, col_start_idx: col_end_idx] = M_1[row, col] * D_N_col_M_prev
+                    M_N[row_start_idx:row_end_idx, col_start_idx:col_end_idx] = (
+                        M_1[row, col] * D_N_col_M_prev
+                    )
             return M_N
+
 
 if __name__ == "__main__":
     A = np.identity(2)
-    B = np.random.uniform(size=(2,2))
-    C = np.random.uniform(size=(2,2))
+    B = np.random.uniform(size=(2, 2))
+    C = np.random.uniform(size=(2, 2))
     fractal_fft = FractalFFT(A, B, C)
-    print(fractal_fft.E(2,1).shape)
-    print(fractal_fft.D(2,1).shape)
+    print(fractal_fft.E(2, 1).shape)
+    print(fractal_fft.D(2, 1).shape)
     print(fractal_fft.M(2))
