0bd372310bbbdc83874db6cbde1820ecae12bc4d
Daniel Tan <dtch009@gmail.com>
2021-10-17 02:09:57 +0800

    Fix mypy errors in tests

--- tests/test_fractal_fft.py
+++ tests/test_fractal_fft.py
@@ -4,7 +4,7 @@ import numpy as np
 from fractal_fft.fractal_fft import FractalFFT
 
 
-def test_fractal_fft():
+def test_fractal_fft() -> None:
     """Check that FractalFFT runs with no errors on a toy example."""
     A = 2 * np.identity(2, dtype=np.int32)
     B = np.identity(2, dtype=np.int32)
