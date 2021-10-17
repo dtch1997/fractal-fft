2c116551a871503ae870fbdeab4b2c14555e099b
Daniel Tan <dtch009@gmail.com>
2021-10-17 16:50:41 +0800

    Fix error in calculation of D(N,m)

--- src/fractal_fft/fractal_fft.py
+++ src/fractal_fft/fractal_fft.py
@@ -108,9 +108,6 @@ class FractalFFT:
     def _D(self, N: int, m: int) -> np.ndarray:
         if N == 1:
             return np.identity(1)
-        elif m == 0:
-            # This is true because b_0 is always fixed to be 0
-            return np.identity(self.K)
         else:
             return np.kron(self.D(N - 1, m), self.E(N, m))  # type: ignore
 
